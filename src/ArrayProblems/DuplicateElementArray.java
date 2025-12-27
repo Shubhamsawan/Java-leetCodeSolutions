d84e557e37b45c7e4e89e02ae30349b357b6909b
Shubham Sawant <sawant0420@gmail.com>
2025-12-27 16:35:40 +0530

    SOlution Part 1.1

---
+++ src/ArrayProblems/DuplicateElementArray.java
@@ -0,0 +1,58 @@
+package ArrayProblems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class DuplicateElementArray {
+
+    // Finds duplicates by modifying the array
+// Works only if elements are in range 0 to n-1
+    public int[] duplicateElementWithoutSpace(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            int index = Math.abs(arr[i]);
+            // If value at index is already negative, it's a duplicate
+            if (arr[index] < 0) {
+                System.out.println("1.1 Duplicate: " + index);
+            } else {
+                // Mark as visited
+                arr[index] = -arr[index];
+            }
+        }
+
+        return arr;
+    }
+
+
+    public int[] FindDuplicatesWithSpace(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();       // stores unique elements
+        ArrayList<Integer> duplicates = new ArrayList<>(); // stores duplicates
+
+
+        for (int num : arr) {
+            if (!set.add(num)) {
+                duplicates.add(num);
+                System.out.println("Duplicate: " + num);
+            }
+        }
+        int[] result = new int[duplicates.size()];
+        for (int i = 0; i < duplicates.size(); i++) {
+            result[i] = duplicates.get(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        DuplicateElementArray duplicateElementArrayObj = new DuplicateElementArray();
+
+        int[] reverseArray = {1, 2, 3, 1, 3, 6, 6};
+        int[] duplicateElementWithoutSpace = duplicateElementArrayObj.duplicateElementWithoutSpace(reverseArray);
+        System.out.println(" duplicateElementWithoutSpace " + Arrays.toString(duplicateElementWithoutSpace));
+
+        int[] reverseArrays = {1, 2, 3, 1, 3, 6, 6};
+        int[] duplicateElementWithSpace = duplicateElementArrayObj.FindDuplicatesWithSpace(reverseArrays);
+        System.out.println(" duplicateElementWithSpace " + Arrays.toString(duplicateElementWithSpace));
+    }
+}
--- src/ArrayProblems/SimpleArrays.java
+++ src/ArrayProblems/SimpleArrays.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 public class SimpleArrays {
 
+    //Logics
     public int[] reverse(int[] reversal) {
         int start = 0, end = reversal.length - 1;
 
@@ -21,30 +22,35 @@ public class SimpleArrays {
         return reversal;
     }
 
-    public int largeElement( int[] largeEle){
+    public int largeElement(int[] largeEle) {
 
-        int max =largeEle[0];
-        for(int i = 0; i < largeEle.length; i++){
-            if(largeEle[i] > max){
+        int max = largeEle[0];
+        for (int i = 0; i < largeEle.length; i++) {
+            if (largeEle[i] > max) {
                 max = largeEle[i];
             }
         }
         return max;
     }
 
+
+
+
     public static void main(String[] args) {
         int[] reversal = {1, 2, 3, 4};
         SimpleArrays simpleArrays = new SimpleArrays();
+        // Reverse Array
         int[] reverse = simpleArrays.reverse(reversal);
         System.out.println(" reverse " + Arrays.stream(reverse).mapToObj(String::valueOf).collect(Collectors.joining(" ")));
 
+        //    Arrays.stream(int[]) → creates an IntStream
+        //    mapToObj(String::valueOf) → converts int to String
+        //    Collectors.joining(" ") → joins elements with space
+
+        //Largest element
         int largestArr = simpleArrays.largeElement(reversal);
         System.out.println(" largestElement  " + largestArr);
     }
-
-//    Arrays.stream(int[]) → creates an IntStream
-//    mapToObj(String::valueOf) → converts int to String
-//    Collectors.joining(" ") → joins elements with space
 
 
 }
